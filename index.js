fbed00d4bee5bac8b7838b88ce983fd9ea4dd31c
Pavan S Patil <psp3075@gmail.com>
2022-12-08 14:15:48 +0530

    get links

--- index.js
+++ index.js
@@ -12,7 +12,15 @@ async function run() {
   //   console.log(html);
 
   const title = await page.evaluate(() => document.title);
-  console.log(title);
+  //console.log(title);
+
+  const text = await page.evaluate(() => document.body.innerText);
+  //console.log(text);
+
+  const links = await page.evaluate(() =>
+    Array.from(document.querySelectorAll("a"), (e) => e.href)
+  );
+  console.log(links);
 
   await browser.close();
 }
